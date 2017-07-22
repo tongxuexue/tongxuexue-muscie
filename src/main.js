5e39d702d237f157b313563032fbc8ba3b0da2c0
gy <yuantxue@126.com>
2017-07-22 17:51:01 +0800

    second submit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 
 /* eslint-disable no-new */
 new Vue({
