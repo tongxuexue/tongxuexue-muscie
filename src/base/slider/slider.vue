10c5929381545205202e009e9f6f6a570e3d9c13
gy <yuantxue@126.com>
2017-07-25 23:41:29 +0800

    add slider

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,42 +1,14 @@
 <template>
-  <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper">
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-        </ul>
-      </div>
+  <div class="slider">
+    <div class="slider-group">
+      <slot>
+      </slot>
     </div>
+    <div class="dots"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import Slider from 'base/slider/slider'
-  import {getRecommend} from 'api/recommend'
-  import {ERR_OK} from 'api/config'
-
-  export default {
-    data() {
-      return {}
-    },
-    created() {
-      this._getRecommend()
-    },
-    methods: {
-      _getRecommend() {
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            console.log(res.data.slider)
-          }
-        })
-      }
-    },
-    components: {
-      Slider
-    }
-  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
@@ -53,8 +25,8 @@
       .slider-wrapper
         position: relative
         width: 100%
-        /*height: 0*/
-        /*padding-top: 40%*/
+        height: 0
+        padding-top: 40%
         overflow: hidden
         .slider-content
           position: absolute
