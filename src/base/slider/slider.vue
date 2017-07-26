c53b1a09c3ec7271c00b2c9ab6eeb7a86353b37a
gy <yuan.gao@ruigushop.com>
2017-07-26 18:38:16 +0800

    add better-scroll

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot>
       </slot>
     </div>
@@ -9,63 +9,103 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import {addClass} from 'common/js/dom'
+
+  export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Boolean,
+        default: true
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth() {
+        this.childern = this.$refs.sliderGroup.children
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.childern.length; i++) {
+          let child = this.childern[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop) {
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snaploop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400,
+          click: true
+        })
+      }
+    }
+
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .recommend
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
-    .recommend-content
-      height: 100%
+  .slider
+    min-height: 1px
+    .slider-group
+      position: relative
       overflow: hidden
-      .slider-wrapper
-        position: relative
-        width: 100%
-        height: 0
-        padding-top: 40%
+      white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
         overflow: hidden
-        .slider-content
-          position: absolute
-          top: 0
-          left: 0
+        text-align: center
+        a
+          display: block
           width: 100%
-          height: 100%
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
-          display: flex
-          box-sizing: border-box
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            flex-direction: column
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
-      .loading-container
-        position: absolute
-        width: 100%
-        top: 50%
-        transform: translateY(-50%)
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      transform: translateZ(1px)
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
+      <div v-if="recommends" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl"/>
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌曲推荐</h1>
@@ -19,7 +26,9 @@
 
   export default {
     data() {
-      return {}
+      return {
+        recommends: []
+      }
     },
     created() {
       this._getRecommend()
@@ -28,6 +37,7 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
             console.log(res.data.slider)
           }
         })
@@ -38,21 +48,7 @@
     }
   }
 </script>
-  export default {
-    created () {
-      this._getRecommend()
-    },
-    methods: {
-      _getRecommend () {
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            console.log(res.data.slider)
-          }
-        })
-      }
-    }
-  }
-</script>
+
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
@@ -62,53 +58,53 @@
     width: 100%
     top: 88px
     bottom: 0
-    .recommend-content
-      height: 100%
+  .recommend-content
+    height: 100%
+    overflow: hidden
+    .slider-wrapper
+      position: relative
+      width: 100%
+      //height: 0
+      //padding-top: 40%
       overflow: hidden
-      .slider-wrapper
-        position: relative
-        width: 100%
-        /*height: 0*/
-        /*padding-top: 40%*/
-        overflow: hidden
-        .slider-content
-          position: absolute
-          top: 0
-          left: 0
-          width: 100%
-          height: 100%
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
-          display: flex
-          box-sizing: border-box
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            flex-direction: column
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
-      .loading-container
+      .slider-content
         position: absolute
+        top: 0
+        left: 0
         width: 100%
-        top: 50%
-        transform: translateY(-50%)
+        height: 100%
+    .recommend-list
+      .list-title
+        height: 65px
+        line-height: 65px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-theme
+      .item
+        display: flex
+        box-sizing: border-box
+        align-items: center
+        padding: 0 20px 20px 20px
+        .icon
+          flex: 0 0 60px
+          width: 60px
+          padding-right: 20px
+        .text
+          display: flex
+          flex-direction: column
+          justify-content: center
+          flex: 1
+          line-height: 20px
+          overflow: hidden
+          font-size: $font-size-medium
+          .name
+            margin-bottom: 10px
+            color: $color-text
+          .desc
+            color: $color-text-d
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
