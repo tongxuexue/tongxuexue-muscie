8a969a339728cbd0e402b7339e4bddf491eeaad8
gy <yuan.gao@ruigushop.com>
2017-08-11 17:44:41 +0800

    fix

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -64,7 +64,7 @@
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   .slide-enter-active, .slide-leave-active
-    transition: all 5s
+    transition: all 0.3s
 
   .slide-enter, .slide-leave-to
     transform: translate3d(100px, 0, 0)
